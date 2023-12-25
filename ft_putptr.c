04236c5262ee60bce306455e6b82738ffa28e872
Selcuk <selcukemnn@gmail.com>
2023-12-25 11:56:51 +0300

    fixed ft_putptr.c when it is null pointer

--- ft_putptr.c
+++ ft_putptr.c
@@ -6,7 +6,7 @@
 /*   By: selcyilm <selcyilm@student.codam.nl>         +#+                     */
 /*                                                   +#+                      */
 /*   Created: 2023/11/25 13:03:32 by selcyilm      #+#    #+#                 */
-/*   Updated: 2023/11/25 14:29:13 by selcyilm      ########   odam.nl         */
+/*   Updated: 2023/12/25 11:55:26 by selcyilm      ########   odam.nl         */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -19,14 +19,14 @@ int	ft_putptr(va_list arg)
 
 	p = va_arg(arg, unsigned long int);
 	if (!p || p == 0)
-		return (write(1, "0x0", 3));
+		return (ft_putstr("(nil)"));
 	return (write(1, "0x", 2) + ft_putpos(p, 'p'));
 }
 
-int main(int ac, char **av)
+/*int main(int ac, char **av)
 {
 	int i = atoi(av[1]);
 	int *p = &i;
 	ft_printf("Mine: %p\n", p);
 	printf("Orig: %p\n", p);
-}
+}*/
